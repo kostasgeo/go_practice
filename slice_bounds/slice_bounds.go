90d5824cc74e2eb49eef0c30098fb06b3e2f0c91
Konstantinos Georgakilas <k.georgakilas@trebbble.co>
2016-05-05 12:07:44 +0300

    add slice_bounds package

---
+++ slice_bounds/slice_bounds.go
@@ -0,0 +1,16 @@
+package slice_bounds 
+
+import "fmt"
+
+func Do(){
+	s := []int{2, 3, 5, 7, 11, 13}
+
+	s = s[1:4]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
+}
