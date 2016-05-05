6da003010303aa83c2b7017aaeca678e345b98d3
Konstantinos Georgakilas <k.georgakilas@trebbble.co>
2016-05-05 11:59:12 +0300

    add slice_literals package and update main to use it

--- main/main.go
+++ main/main.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 import "github.com/kostasgeo/slice_pointers"
+import "github.com/kostasgeo/slice_literals"
 
 func main() {
 	fmt.Println("Go main Function")
+	fmt.Println("slice_pointers")
 	slice_pointers.Do()
+	fmt.Println("slice_literals")
+	slice_literals.Do()
 }
---
+++ slice_literals/slice_literals.go
@@ -0,0 +1,25 @@
+// creates arrays and builds slices that reference them
+package slice_literals 
+
+import "fmt"
+
+func Do() {
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int 
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+}
